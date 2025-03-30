30a6c99359bacab26523643ccbee9511fc94e4a6
Nico <nicolascaorsi@gmail.com>
2025-03-30 01:15:23 +0000

    chore: alterado parser do postgres para converter o tipo number para numero

--- customers-favorite-products-api/src/config/typeorm.data-source-options.config.ts
+++ customers-favorite-products-api/src/config/typeorm.data-source-options.config.ts
@@ -1,12 +1,16 @@
 import { CustomerFavoritedProductTypeOrm } from '@customers/persistence/typeorm/favorited-product.typeorm';
-import { registerAs } from '@nestjs/config';
 import { ProductTypeOrm } from '@products/persistence/typeorm/product.typeorm';
+import { types } from 'pg';
 import { DataSourceOptions } from 'typeorm';
 import { CustomerTypeOrm } from '../features/customers/persistence/typeorm/customer.typeorm';
+
+// Configurando o parser do tipo numeric para float, pois por padrão o driver configura para string,
+// uma vez que o tamanho máximo numérico do javascript não pode ser representado no postgres,
+// porém estamos trabalhando com valores menores, então é seguro converter para float
+types.setTypeParser(1700, parseFloat);
+
 export const defaultDataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
   entities: [CustomerTypeOrm, ProductTypeOrm, CustomerFavoritedProductTypeOrm],
   logging: process.env.TYPEORM_LOGGING == 'true',
 };
-export default registerAs('database', () => defaultDataSourceOptions);
